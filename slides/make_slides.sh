fd111d570cbcbdedeac99d74fb96946f90f6ff78
Enrico <e.riccardi82@gmail.com>
2024-01-22 17:07:25 +0100

    handout

--- slides/make_slides.sh
+++ slides/make_slides.sh
@@ -22,17 +22,15 @@ preprocess -DFORMAT=pdflatex ../newcommands.p.tex > newcommands_keep.tex
 doconce format pdflatex $filename --latex_title_layout=beamer --latex_table_format=footnotesize --latex_admon_title_no_period --latex_code_style=pyg 
 doconce slides_beamer $filename --beamer_slide_theme=red_shadow
 pdflatex -shell-escape $filename
-pdflatex -shell-escape $filename
-pdflatex -shell-escape $filename
-cp ${filename}.pdf ${filename}-beamer.pdf
-rm -f ${filename}.pdf
-cp ${filename}.tex ${filename}-beamer.tex  # sometimes nice to look at
+mv ${filename}.pdf ${filename}-beamer.pdf
 
 # Handouts
-# doconce format pdflatex $filename --latex_title_layout=beamer --latex_table_format=footnotesize --latex_admon_title_no_period --latex_code_style=pyg
-# doconce slides_beamer $filename --beamer_slide_theme=red_shadow --handout
-# pdflatex -shell-escape $filename
-# pdflatex -shell-escape $filename
-# pdflatex -shell-escape $filename
-# pdfnup --nup 2x3 --frame true --delta "1cm 1cm" --scale 0.9 --outfile ${filename}-beamer-handouts2x3.pdf ${filename}.pdf
-# rm -f ${filename}.pdf
+
+
+sed 's/!bpop//' $filename.do.txt | 's/!epop//' > tmp_$filename.do.txt
+doconce slides_beamer tmp_$filename --beamer_slide_theme=red_shadow --handout
+pdflatex -shell-escape tmp_$filename
+mv tmp_${filename}.pdf ${filename}-beamer.pdf
+pdfjam ${filename}-beamer.pdf --no-landscape --frame true --nup 2x4 --suffix 8up
+
+
