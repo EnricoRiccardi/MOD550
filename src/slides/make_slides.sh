83c4d3bb3b231f43f43f3416c2db44b7aea4ff8b
Enrico <e.riccardi82@gmail.com>
2024-02-05 13:14:35 +0100

    restructuring

--- src/slides/make_slides.sh
+++ src/slides/make_slides.sh
@@ -31,4 +31,3 @@ pdflatex -shell-escape tmp_preprocess__$filename.tex
 pdfjam tmp_preprocess__${filename}.pdf --no-landscape --frame true --nup 2x4 --suffix 8up
 mv tmp_preprocess__${filename}-8up.pdf ${filename}-8up.pdf
 rm tmp* 
-./clean.sh
