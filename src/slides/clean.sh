b327086d5a85a6a0077b157378db4ed73056e9d3
Enrico <e.riccardi82@gmail.com>
2024-02-12 17:07:53 +0100

    up

--- src/slides/clean.sh
+++ src/slides/clean.sh
@@ -7,3 +7,4 @@ rm -rf _build __pycache__ _mint*
 rm -rf *.aux *.dlog *.html *.idx *.log *.out tmp_* *.old* *.tex
 rm -rf *.nav *.pyg *.snm *.vrb *.toc *.sty
 rm -rf .ipyn* .dlog *copyright .*copyright .tmp*
+rm -rf ../figures/*pdf
--- src/slides/make_slides.sh
+++ src/slides/make_slides.sh
@@ -6,6 +6,11 @@
 # chapter and slides.
 
 dofile=$1
+if [[ -z "$dofile" ]]; then
+  for i in *.do.txt; do
+    dofile=$i
+  done
+fi
 if [ ! -f $dofile ]; then
   echo "No such file: $dofile"
   exit 1
@@ -28,7 +33,7 @@ mv ${filename}.pdf ${filename}-beamer.pdf
 doconce format pdflatex tmp_preprocess__$filename --latex_title_layout=beamer --latex_table_format=footnotesize --latex_admon_title_no_period --latex_code_style=pyg 
 doconce slides_beamer tmp_preprocess__$filename --beamer_slide_theme=red_shadow --handout
 pdflatex -shell-escape tmp_preprocess__$filename.tex
-pdfjam tmp_preprocess__${filename}.pdf --no-landscape --frame true --nup 2x4 --suffix 8up
-mv tmp_preprocess__${filename}-8up.pdf ${filename}-8up.pdf
+pdfjam tmp_preprocess__${filename}.pdf --no-landscape --frame true --nup 2x3 --suffix 6up
+mv tmp_preprocess__${filename}-6up.pdf ${filename}-6up.pdf
 rm tmp* 
 ./clean.sh
