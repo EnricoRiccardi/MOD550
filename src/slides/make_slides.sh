935e505a030d515b9f2b678cbc1ab56ac91104a6
EnricoRiccardi <e.riccardi82@gmail.com>
2024-02-25 23:22:44 +0100

    class 26.2.2024

--- src/slides/make_slides.sh
+++ src/slides/make_slides.sh
@@ -4,19 +4,16 @@
 #
 # But this script is normally run from make.sh to make both
 # chapter and slides.
+set -e 
 
 dofile=$1
-if [[ -z "$dofile" ]]; then
-  for i in *.do.txt; do
-    dofile=$i
-  done
-fi
-if [ ! -f $dofile ]; then
-  echo "No such file: $dofile"
+class=MOD550
+if [ ! -f class-$dofile-$class.do.txt ]; then
+  echo "No such file: class-$dofile-$class"
   exit 1
 fi
 
-filename=`echo $dofile | sed 's/\.do\.txt//'`
+filename=`echo class-$dofile-$class | sed 's/\.do\.txt//'`
 
 # LaTeX PDF for printing
 # (Smart to make this first to detect latex errors - HTML/MathJax
@@ -24,13 +21,13 @@ filename=`echo $dofile | sed 's/\.do\.txt//'`
 
 # LaTeX Beamer
 preprocess -DFORMAT=pdflatex ../newcommands.p.tex > newcommands_keep.tex
-doconce format pdflatex $filename --latex_title_layout=beamer --latex_table_format=footnotesize --latex_admon_title_no_period --latex_code_style=pyg 
+doconce format pdflatex $filename --latex_title_layout=beamer --latex_table_format=footnotesize --latex_admon_title_no_period --latex_code_style=pyg --no_ampersand_quote 
 doconce slides_beamer $filename --beamer_slide_theme=red_shadow
 pdflatex -shell-escape $filename
 mv ${filename}.pdf ${filename}-beamer.pdf
 
 # Handouts
-doconce format pdflatex tmp_preprocess__$filename --latex_title_layout=beamer --latex_table_format=footnotesize --latex_admon_title_no_period --latex_code_style=pyg 
+doconce format pdflatex tmp_preprocess__$filename --latex_title_layout=beamer --latex_table_format=footnotesize --latex_admon_title_no_period --latex_code_style=pyg --no_ampersand_quote 
 doconce slides_beamer tmp_preprocess__$filename --beamer_slide_theme=red_shadow --handout
 pdflatex -shell-escape tmp_preprocess__$filename.tex
 pdfjam tmp_preprocess__${filename}.pdf --no-landscape --frame true --nup 2x3 --suffix 6up
